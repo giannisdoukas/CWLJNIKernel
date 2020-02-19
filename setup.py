6a38d717830565c5601c8de1b7740d4bfe442138
Giannis Doukas <giannisdoukas2311@gmail.com>
2020-02-19 18:07:13 +0000

    init setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='cwlkernel',
+    version='0.1',
+    py_modules=['cwlkernel'],
+    url='https://github.com/giannisdoukas/CWLJNIKernel',
+    author='Yannis Doukas',
+    author_email='',
+    description='',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+)
