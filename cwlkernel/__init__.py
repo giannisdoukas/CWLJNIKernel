86c1d09a0bdba1e968eb1270a7803e9aca67bb51
Giannis Doukas <giannisdoukas2311@gmail.com>
2020-06-04 18:06:39 +0100

    add version

--- cwlkernel/__init__.py
+++ cwlkernel/__init__.py
@@ -1,2 +1,4 @@
 # noinspection PyUnresolvedReferences
 from . import kernel_magics
+
+__version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
+import codecs
+import os.path
+
 from jupyter_client.kernelspec import KernelSpecManager
 from setuptools import setup
 
 name = 'cwlkernel'
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name=name,
-    version='0.0.1',
+    version=get_version(f"{name}/__init__.py"),
     packages=['cwlkernel', 'cwlkernel.cwlrepository'],
     url='https://github.com/giannisdoukas/CWLJNIKernel',
     author='Yannis Doukas',
