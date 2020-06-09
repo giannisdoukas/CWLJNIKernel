94b8a5f7420a0a76ed532d746963fc6c03e62827
Giannis Doukas <giannisdoukas2311@gmail.com>
2020-06-09 18:48:23 +0100

    add post commands in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,42 @@ import os.path
 
 from jupyter_client.kernelspec import KernelSpecManager
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 name = 'cwlkernel'
+
+
+def install_kernel_specs():
+    import sys
+    kernel_requirements_directory = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'kernelmeta'
+    )
+
+    print('Installing IPython kernel spec')
+    KernelSpecManager().install_kernel_spec(
+        kernel_requirements_directory,
+        name,
+        user=False,
+        prefix=sys.prefix
+    )
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        develop.run(self)
+        install_kernel_specs()
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+        install_kernel_specs()
 
 
 def read(rel_path):
@@ -22,7 +56,7 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 
-with open("README.md", "r") as fh:
+with open(os.sep.join([os.path.abspath(os.path.dirname(__file__)), "README.md"]), "r") as fh:
     long_description = fh.read()
 
 setup(
@@ -44,20 +78,8 @@ setup(
         "Development Status :: 2 - Pre-Alpha",
         "Framework :: IPython",
     ],
-)
-
-import sys
-import os
-
-kernel_requirements_directory = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'kernelmeta'
-)
-
-print('Installing IPython kernel spec')
-KernelSpecManager().install_kernel_spec(
-    kernel_requirements_directory,
-    name,
-    user=False,
-    prefix=sys.prefix
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
 )
